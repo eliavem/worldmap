e112c2cff0301ef51d389ae87e5ae0035b59f849
eliavem <64979576+eliavem@users.noreply.github.com>
2024-03-19 20:45:21 -0700

    delete city

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -56,7 +56,22 @@ function CitiesProvider({ children }) {
       // global state synchronized
       setCities((cities) => [...cities, data]);
     } catch {
-      alert("There was an error loading daat...");
+      alert("There was an error creating city");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function deleteCity(id) {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch {
+      alert("There was an error deleting city");
     } finally {
       setIsLoading(false);
     }
@@ -64,7 +79,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
